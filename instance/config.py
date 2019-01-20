b99e3446f64610a1d457f83f227fd13ecf9d2ead
Nappy29 <ewaneenombe@gmail.com>
2019-01-20 01:28:47 +0100

    this is fine

--- instance/config.py
+++ instance/config.py
@@ -4,4 +4,4 @@ WATSON_API_KEY = 'ynr8PPMiNa_NfW_giS6ER1MKlEjB03dTR2JbZfe3QPpo'
 WATSON_API_URL = 'https://gateway-lon.watsonplatform.net/speech-to-text/api'
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
-UPLOAD = os.getcwd() + '/instance/uploads/'
+UPLOAD = os.getcwd() + '/tmp/'
